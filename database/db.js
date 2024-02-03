e153158f00eabf268621f53b8aa98c363e1538e1
Himanshu Chauhabn <hc7456947@gmail.com>
2024-02-04 02:44:42 +0530

    connected database

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 
 const DBConnection = async () => {
-    const MONODB_URI = `mongodb+srv://himanshu:hc7456947@file-sharing.n6hr7jv.mongodb.net/?retryWrites=true&w=majority`;
+    const MONODB_URI = `mongodb://himanshu:hc7456947@ac-aijubev-shard-00-00.n6hr7jv.mongodb.net:27017,ac-aijubev-shard-00-01.n6hr7jv.mongodb.net:27017,ac-aijubev-shard-00-02.n6hr7jv.mongodb.net:27017/?ssl=true&replicaSet=atlas-5o8znf-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONODB_URI, { useNewUrlParser: true });
         console.log('Database connected successful');
